a191409b5925c20b4267380844cec37999db6f62
Luan Rocha <rochas.luan@gmail.com>
2020-10-28 23:55:14 -0300

    Adding database access

---
+++ src/util/Utils.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Utils {
+	public static Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+	}
+
+	public static Department instantiateSellerDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
+	
+	public static Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("Name"));
+		return dep;
+	}
+}
